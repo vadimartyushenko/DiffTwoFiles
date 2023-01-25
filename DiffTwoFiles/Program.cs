1d8439dde5ba8a3fac06c0cd3be35c556743dd85
Vadim Artyshenko <artushenkomail@mail.ru>
2023-01-25 18:53:59 +0700

    update

--- DiffTwoFiles/Program.cs
+++ DiffTwoFiles/Program.cs
@@ -1,23 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+if (args.Length != 2) {
+    Console.WriteLine("Only two paths required!");
+    return;
+}
 
-var filePath1 = args[0];
-var filePath2 = args[1];
-if (!(File.Exists(filePath1) && File.Exists(filePath2))) {
+var fi1 = new FileInfo(args[0]);
+var fi2 = new FileInfo(args[1]);
+if (!(fi1.Exists && fi2.Exists)) {
     Console.WriteLine($"Not found input file!");
 }
 
-var text1 = File.ReadAllLines(filePath1);
-var text2 = File.ReadAllLines(filePath2);
+var text1 = File.ReadAllLines(fi1.FullName);
+var text2 = File.ReadAllLines(fi2.FullName);
 
 var pt_numbers = text1.Select(str => str.Trim()).ToArray();
 
 var stats_numbers = text2.Select(str => str.Trim()).ToArray();
 
-var uniq_numbers = pt_numbers.Except(stats_numbers).ToArray();
+var f1_uniq_numbers = pt_numbers.Except(stats_numbers).ToArray();
 
-foreach (var num in uniq_numbers)
+if (f1_uniq_numbers.Length > 0)
 {
-    Console.WriteLine(num);
+    Console.WriteLine($"Uniq strings for file \"{fi1.Name}\"");
+    foreach (var num in f1_uniq_numbers)
+    {
+        Console.WriteLine(num);
+    }
+} else {
+    var f2_uniq = stats_numbers.Except(pt_numbers).ToArray();
+    if (f2_uniq.Length > 0) {
+        Console.WriteLine($"Uniq strings for file \"{fi2.Name}\"");
+        foreach (var num in f2_uniq) {
+            Console.WriteLine(num);
+        }
+    }
 }
+
 Console.ReadLine();
